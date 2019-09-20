c7b465ad4d1cd5f85ba05d2d165abae25041df88
Rez Delamasa <rezdelamasa@gmail.com>
2019-09-20 00:44:45 -0500

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,12 +1,36 @@
-export default {
+const dev = {
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://ohjnstaaua.execute-api.us-east-2.amazonaws.com/prod"
+    URL: "https://bkmqpk3zdf.execute-api.us-east-2.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_UdgYTod0a",
-    APP_CLIENT_ID: "6ajnta6m1slp1e71ldqsg15pe2",
-    IDENTITY_POOL_ID: "us-east-2:ec8ae969-123f-420a-acb8-dd832e2d850b"
+    USER_POOL_ID: "us-east-2_iltSlQP4o",
+    APP_CLIENT_ID: "7eumi89ao0b4a0d2hl3ri015io",
+    IDENTITY_POOL_ID: "us-east-2:78486375-ef6a-4e65-85b6-e5e0af8684cf"
   }
+};
+
+const prod = {
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://2hxlw9ac0m.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_hlXiA5eJn",
+    APP_CLIENT_ID: "4nckfbn7pf5h66l50pc0o2jnpv",
+    IDENTITY_POOL_ID: "us-east-2:170dcec4-1474-45c6-bf3b-29ef8cc74716"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
